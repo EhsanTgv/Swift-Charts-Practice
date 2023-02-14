a5cfe3e9e46d912d63a5d16eeddf3424f135cae4
EhsanTgv <EhsanTgv@gmail.com>
2023-02-14 15:11:46 +0300

    Initial Commit

---
+++ Swift Charts Practice/Swift_Charts_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Charts_PracticeApp.swift
+//  Swift Charts Practice
+//
+//  Created by Ehsan Taghavi on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Charts_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
